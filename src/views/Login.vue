ef0bf85577dedd20ab2849614e7ca7bc67e2d202
Kunniii <57139436+Kunniii@users.noreply.github.com>
2023-12-29 22:14:20 +0700

    added option to create a new api key

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -13,13 +13,22 @@ function saveKey() {
 <template>
   <div class="flex flex-col px-2 w-80 h-96 bg-white rounded-xl drop-shadow-xl mt-[20vh]">
     <h1 class="text-center pt-5">Welcome to my Gemini AI Client!</h1>
-    <h2 class="text-3xl text-center font-bold pb-5 pt-4">Use your API key</h2>
+    <h2 class="text-3xl text-center font-bold pb-4">Use your API key</h2>
     <input
       type="password"
       v-model="input"
       placeholder="API KEY"
-      class="border-2 border-dashed hover:border-solid focus:outline-emerald-500 border-emerald-300 rounded-full px-4 py-2 my-5 mx-4"
+      class="border-2 border-dashed hover:border-solid focus:outline-emerald-500 border-emerald-300 rounded-full px-4 py-2 mb-2 mx-4"
     />
+    <span class="text-center">
+      <p class="text-gray-500">or</p>
+      <a
+        href="https://makersuite.google.com/app/apikey"
+        target="_blank"
+        class="underline text-green-600"
+        >create one</a
+      >
+    </span>
     <button
       @click="saveKey"
       class="bg-green-500 px-10 py-2 my-5 rounded-full text-white font-bold hover:bg-green-600 text-xl mx-auto"
